d067473e77c5133102bb754ec4458243e0198d0b
jurrutiag <juan.urrutia.gandolfo@gmail.com>
2019-09-06 14:58:06 -0400

    Deleted Angles.py

--- Modelo/RoboticManipulator.py
+++ Modelo/RoboticManipulator.py
@@ -13,9 +13,16 @@ class RoboticManipulator():
     def anglesToPosition(self, theta_1, theta_2, theta_3, theta_4):
         """
 
-        :param angles: Angles for the arm to move to.
-        :type angles: Angles
-
+        :param theta_1: Angle from X to the arm on XY plane, A1 origin
+        :type theta_1: double
+        :param theta_2: Angle from X to L2 on XZ plane, A1 origin
+        :type theta_2: double
+        :param theta_3: Angle from X to L3 on XZ plane, A2 origin
+        :type theta_3: double
+        :param theta_4: Angle from X to L4 on XZ plane, A3 origin
+        :type theta_4: double
+        :return: None
+        :rtype: None
         """
 
         XYprojection = self._L2 * math.cos(theta_1) + self._L3 + self._L4 * math.cos(theta_4)
