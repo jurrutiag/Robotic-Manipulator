e8745209fe3fc5186a1d37aa215de3a109226b6b
jurrutiag <juan.urrutia.gandolfo@gmail.com>
2019-09-07 20:15:19 -0400

    Added length property to Blender model

--- Blender/BlenderDriver.py
+++ Blender/BlenderDriver.py
@@ -4,13 +4,25 @@ from math import radians
 
 class BlenderDriver:
 
-    def __init__(self, theta):
+    def __init__(self, theta, target, size):
         self._theta = theta
+        self._target = target
+        self._size = size
+        self._dimension2scale = 1/2
 
     def execute(self):
         A1 = bpy.data.objects["Aone"]
         A2 = bpy.data.objects["Atwo"]
         A3 = bpy.data.objects["Athree"]
+
+        L1 = bpy.data.objects["Lone"]
+        L2 = bpy.data.objects["Ltwo"]
+        L3 = bpy.data.objects["Lthree"]
+        L4 = bpy.data.objects["Lfour"]
+
+        TargetElement = bpy.data.objects["Target"]
+
+        ## Initial configurations
 
         A1.animation_data_clear()
         A2.animation_data_clear()
@@ -29,9 +41,21 @@ class BlenderDriver:
         A2.rotation_euler = (0, 0, 0)
         A3.rotation_euler = (0, 0, 0)
 
+        L1.scale = (0.5, 0.5, self._size[0] * self._dimension2scale)
+        L2.scale = (0.5, 0.5, self._size[1] * self._dimension2scale)
+        L3.scale = (0.5, 0.5, self._size[2] * self._dimension2scale)
+
+        A1.location = (0, 0, -self._size[0])
+        A2.location = (0, 0, -self._size[1])
+        A3.location = (0, 0, self._size[2])
+
+        TargetElement.location = (self._target[0], self._target[1], self._target[2])
+
         A1.keyframe_insert(data_path="rotation_euler", index=-1)
         A2.keyframe_insert(data_path="rotation_euler", index=-1)
         A3.keyframe_insert(data_path="rotation_euler", index=-1)
+
+        TargetElement.keyframe_insert(data_path="location", index=-1)
 
         rotation1 = [(0, 0, radians(theta_1)), A1]
         rotation2 = [(radians(theta_2), 0, radians(theta_1)), A1]
@@ -39,6 +63,8 @@ class BlenderDriver:
         rotation4 = [(radians(theta_4), 0, 0), A3]
 
         rotations = (rotation1, rotation2, rotation3, rotation4)
+
+
 
         for rotation in rotations:
             frame_num += 10
@@ -51,5 +77,5 @@ class BlenderDriver:
 
 
 if __name__ == "__main__":
-    driver = BlenderDriver((90, -30, 10, -20))
+    driver = BlenderDriver((90, -30, 10, -20), (20, 10, 10), (10, 5, 5, 5))
     driver.execute()
