fd8c58ae399b342085e74dae0e01d60b1a053987
jamosnaim <jamosnaim@hotmail.com>
2019-09-25 21:19:37 -0300

    Update Initialization.py
    more corrections to intialization

--- Model/Initialization.py
+++ Model/Initialization.py
@@ -1,22 +1,21 @@
 import math
 import numpy as np
 import random
-import Individual
 
 def initialization(populationSize, initialAngles, finalAngles, armPivots, steps):
-    P = np.zeros((populationSize, armPivots))
+    P = np.zeros((steps, armPivots))
     results = []
     for ind in range(populationSize):
         for h in range(armPivots):
             #solo el extremo inicial esta fijo
-            average = random.randrange(2, populationSize) 
-            std = random.randrange(1,populationSize/6)
+            average = random.randrange(2, steps) 
+            std = random.randrange(1,steps/6)
             R= abs(initialAngles[h]-finalAngles[h])
             
-            for i in range(populationSize):
+            for i in range(steps):
                 #no estoy seguro si habra que poner step distinto
                 noise = random.randrange(-3*R, 3*R)*math.exp(-(i-average)**2/(2*std**2))       
-                P[i,h] = initialAngles[h] + (i-1)*(initialAngles[h]*finalAngles[h])/(populationSize-1)+noise
+                P[i,h] = initialAngles[h] + (i-1)*(initialAngles[h]*finalAngles[h])/(steps-1)+noise
 
 
         results.append(P)
