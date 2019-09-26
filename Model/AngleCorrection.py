cd56bb491a827ec2962c965d173ef9a20f527269
jamosnaim <jamosnaim@hotmail.com>
2019-09-25 21:41:26 -0300

    Create AngleCorrection.py
    angle correction added

---
+++ Model/AngleCorrection.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+import random
+import Individual
+def angleCorrection(minAngles, maxAngles, individuals):
+
+    for ind in individuals:
+        for i in individuals.shape(0):
+            for h in shape(1):
+                dif = abs(individuals[i,h]-maxAngles[h])
+                if individuals[i,h]>maxAngles[h]:
+                    individuals[i,h] =maxAngles[h] - dif
+                elif individuals[i,h]<minAngles[h]:
+                    individuals[i,h] =maxAngles[h] + dif
+            
