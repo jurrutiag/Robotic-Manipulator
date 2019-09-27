d7dba8f6dd640ff3d26c4823d1581b941e0be968
jurrutiag <juan.urrutia.gandolfo@gmail.com>
2019-09-26 21:50:45 -0300

    debug

--- Model/GeneticAlgorithm.py
+++ Model/GeneticAlgorithm.py
@@ -9,7 +9,7 @@ import time
 
 class GeneticAlgorithm:
 
-    def __init__(self, desired_position, manipulator, pop_size=100, cross_individual_prob=0.8, mut_individual_prob=0.01, cross_joint_prob=0.5, mut_joint_prob=0.5, pairing_prob=0.5, sampling_points=50, torques_ponderations=(1, 1, 1, 1), generation_threshold = 200, fitness_threshold = 1, progress_threshold = 1, generations_progress_threshold = 50):
+    def __init__(self, desired_position, manipulator, pop_size=100, cross_individual_prob=0.6, mut_individual_prob=0.05, cross_joint_prob=0.5, mut_joint_prob=0.5, pairing_prob=0.5, sampling_points=50, torques_ponderations=(1, 1, 1, 1), generation_threshold = 200, fitness_threshold = 1, progress_threshold = 1, generations_progress_threshold = 50):
 
         # Algorithm parameters
 
@@ -67,6 +67,7 @@ class GeneticAlgorithm:
 
         # First generation fitness
         self.evaluateFitness(self._population)
+        # print(self._population[0].getFitness(), self._population[1].getFitness())
         self.getBestAndAverage()
 
         while True:
@@ -108,7 +109,6 @@ class GeneticAlgorithm:
 
     def initialization(self):
 
-
         results = []
 
         finalAngles = np.pi * np.random.random(size=(self._pop_size, 4)) - np.pi/2
@@ -126,7 +126,7 @@ class GeneticAlgorithm:
                 R = abs(self._initial_angles[h] - finalAngles[ind][h])
 
                 P[0, h] = self._initial_angles[h]
-                for i in range(2, self._sampling_points):
+                for i in range(2, self._sampling_points + 1):
                     #no estoy seguro si habra que poner step distinto
                     # A = (6 * R) * np.random.random() - 3 * R
                     # noise = A * math.exp(-(i - average) ** 2 / (2 * std ** 2))
